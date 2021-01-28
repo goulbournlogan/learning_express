a06f116ce67fe60e3c2af15fa73a394463ab5b19
Logan Goulbourn <goulbournlogan@gmail.com>
2021-01-28 12:48:16 +0000

    adding .gitignore, package-lock.json, package.json and src folder

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+    console.log("test123");
+    res.send("hqwewqerr");
+});
+
+app.get("/data", (req, res) => {
+    console.log(req.query)
+    res.send({ message: req.query});
+});
+
+ app.get("/something", (req, res) => {
+     res.send({name: "logan", age:35});
+ });
+
+ app.get("/person/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("wqer");
+ })
+
+app.listen(5000, () => {
+    console.log("listening on port 5000")
+});
